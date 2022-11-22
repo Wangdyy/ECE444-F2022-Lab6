3220d03d3d69efd52d0b24490c9fe9dab17d51c6
Wangdyy <wendywang68912@gmail.com>
2022-11-22 15:55:03 -0500

    added login required feature

--- project/app.py
+++ project/app.py
@@ -1,4 +1,6 @@
 import sqlite3
+from functools import wraps
+
 from pathlib import Path
 
 from flask import Flask, g, render_template, request, session, \
@@ -69,6 +71,15 @@ def logout():
     flash('You were logged out')
     return redirect(url_for('index'))
 
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not session.get('logged_in'):
+            flash('Please log in.')
+            return jsonify({'status': 0, 'message': 'Please log in.'}), 401
+        return f(*args, **kwargs)
+    return decorated_function
+
 @app.route('/search/', methods=['GET'])
 def search():
     query = request.args.get("query")
@@ -79,11 +90,13 @@ def search():
 
 
 @app.route('/delete/<int:post_id>', methods=['GET'])
+@login_required
 def delete_entry(post_id):
     """Deletes post from database."""
     result = {'status': 0, 'message': 'Error'}
     try:
-        db.session.query(models.Post).filter_by(id=post_id).delete()
+        new_id = post_id
+        db.session.query(models.Post).filter_by(id=new_id).delete()
         db.session.commit()
         result = {'status': 1, 'message': "Post Deleted"}
         flash('The entry was deleted.')
